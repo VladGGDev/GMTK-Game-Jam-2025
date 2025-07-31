f41ff20369e45ced03093253182eb1c17b6c3576
VladGGDev <vladcotop@gmail.com>
2025-07-31 10:52:21 +0300

    Initialization changes
    Increased resolution from 320x180 to 640x360 and changed fixed update interval from 60fps to 144fps

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import tests.cartestscene, tests.uitestscene
 
 
 # Setup
+engine.unscaled_fixed_delta_time = 1 / 144
 engine.run(
     engine.SceneManager(
         {
@@ -12,7 +13,8 @@ engine.run(
         "Car"
     ),
     {
-        "Main" : engine.DrawPass((320, 180), 0, None, (254, 231, 97, 255)),
-        "UI" : engine.DrawPass.create_ui((320, 180), 99)
+        # "Main" : engine.DrawPass((320, 180), 0, None, (254, 231, 97, 255)),
+        "Main" : engine.DrawPass((640, 360), 0, None, (254, 231, 97, 255)),
+        "UI" : engine.DrawPass.create_ui((640, 360), 99)
     },
     True)
