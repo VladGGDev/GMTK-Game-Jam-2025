1e70d2c7eabe7bfee81deeb427de34610bfee3c7
VladGGDev <vladcotop@gmail.com>
2025-07-31 10:48:39 +0300

    Finished handbrake car implementation

--- game/actors/car.py
+++ game/actors/car.py
@@ -1,17 +1,19 @@
-import engine, pygame, engine.collider as colliders
+import engine, pygame, engine.collider as colliders, engine.tweening.lerpfuncs as lerputil, engine.tweening.easingfuncs as easings
 from math import pi, sin, cos, degrees
 
 
 class Car(engine.Actor):
     def __init__(self):
         # Constants
-        self.MAX_SPEED = 256
-        self.MIN_SPEED = 64
+        self.MAX_SPEED = 150
+        self.MAX_DRIFT_SPEED = 200
+        self.MIN_SPEED = 80
         self.ACCELERATION = self.MAX_SPEED / 0.25
         self.DECELERATION = self.MAX_SPEED / 1
         self.FRICTION_DECELERATION = self.MAX_SPEED / 5
-        self.TURN_SPEED = 160 * pi / 180 # radians per second
-        self.DRIFT_TURN_SPEED = 450 * pi / 180 # radians per second
+        self.TURN_SPEED = 120 * pi / 180 # radians per second
+        self.DRIFT_TURN_SPEED = 270 * pi / 180 # radians per second
+        self.MAX_DRIFT_ENERGY = 1 # Seconds of drift
         
         # Collider initialization
         super().__init__(colliders.CircleCollider(pygame.Vector2(0, 0), 4, "Car"))
@@ -19,27 +21,43 @@ class Car(engine.Actor):
         # Dynamic values
         self.direction = 0
         self.speed = self.MIN_SPEED
+        self.drift_energy = 0
     
     def update(self):
-        # key_down = engine.get_key_down
-        key_held = engine.get_key
+        pressed = engine.get_key
         
-        if key_held(pygame.K_w) or key_held(pygame.K_UP):
+        dir = int(pressed(pygame.K_a) or pressed(pygame.K_LEFT)) - int(pressed(pygame.K_d) or pressed(pygame.K_RIGHT))
+        max_sp = lerputil.lerp(self.MAX_SPEED, self.MAX_DRIFT_SPEED, easings.ease_out_cubic(self.drift_energy / self.MAX_DRIFT_ENERGY))
+        turn_sp = self.TURN_SPEED if not pressed(pygame.K_SPACE) else self.DRIFT_TURN_SPEED
+        
+        # Drift energy
+        self.drift_energy = max(self.drift_energy - engine.delta_time(), 0)
+        if pressed(pygame.K_SPACE) and dir != 0:
+            self.drift_energy = self.MAX_DRIFT_ENERGY
+        
+        
+        # Acceleration controls
+        if pressed(pygame.K_w) or pressed(pygame.K_UP) or self.drift_energy > 0:
             self.speed += self.ACCELERATION * engine.delta_time()
-        elif key_held(pygame.K_s) or key_held(pygame.K_DOWN):
+        elif pressed(pygame.K_s) or pressed(pygame.K_DOWN):
             self.speed -= self.DECELERATION * engine.delta_time()
         else:
             self.speed -= self.FRICTION_DECELERATION * engine.delta_time()
-        self.speed = pygame.math.clamp(self.speed, self.MIN_SPEED, self.MAX_SPEED)            
+        self.speed = pygame.math.clamp(self.speed, self.MIN_SPEED, max_sp)            
         
-        dir = int(key_held(pygame.K_a) or key_held(pygame.K_LEFT)) - int(key_held(pygame.K_d) or key_held(pygame.K_RIGHT))
-        turn = self.DRIFT_TURN_SPEED if key_held(pygame.K_SPACE) else self.TURN_SPEED
-        self.direction += dir * turn * engine.delta_time() * (self.speed / self.MAX_SPEED)
+        # Steering controls
+        self.direction += dir * turn_sp * engine.delta_time() * (self.speed / max_sp)
         
-        engine.draw_passes["Main"].camera.position = self.collider.position
+        # Move camera to position
+        # engine.draw_passes["Main"].camera.position = self.collider.position
+        # Framerate-independent damping
+        engine.draw_passes["Main"].camera.position = lerputil.vector2_lerp(
+            engine.draw_passes["Main"].camera.position,
+            self.collider.position,
+            1 - 0.005**engine.delta_time())
     
     def fixed_update(self):
-        move = self.speed * engine.delta_time()
+        move = self.speed * engine.fixed_delta_time()
         self.collider.position += pygame.Vector2(
             move * -sin(self.direction),
             move * -cos(self.direction)
@@ -48,8 +66,8 @@ class Car(engine.Actor):
     def draw(self):
         engine.draw_passes["Main"].blit(
             99,
-            engine.DrawPass.get_pixel(pygame.Color("red"), (8, 12)),
+            engine.DrawPass.get_pixel(pygame.Color("red" if self.drift_energy > 0 else "blue"), (8, 12)),
             self.collider.position,
             (1, 1),
-            degrees(self.direction + (pi / 1.75 if engine.get_key(pygame.K_SPACE) else 0))
+            degrees(self.direction + (pi / 1.75 if self.drift_energy / self.MAX_DRIFT_ENERGY > 0.5 else 0))
         )
