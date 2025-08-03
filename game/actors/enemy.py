07988bdef38c2913edeaaafef062d1f0a35aeed2
VladGGDev <vladcotop@gmail.com>
2025-08-03 17:14:17 +0300

    Changed zombie sprite

--- game/actors/enemy.py
+++ game/actors/enemy.py
@@ -1,4 +1,5 @@
-import engine, pygame, random
+import engine, pygame, random, math
+from engine.lerputil import remap
 from engine.spritesheet import SpriteSheet
 import engine.collider as collider
 
@@ -7,17 +8,17 @@ from game.actors.decalmanager import DecalManager
 
 
 class Enemy(engine.Actor):
-    graphic = SpriteSheet("game/sprites/Zombie.png", (8, 8))
+    graphic = pygame.image.load("game/sprites/Zombie.png")
     big_blood_tex = SpriteSheet("game/sprites/Blood Big.png", (16, 16))
-    MAX_SPEED = 40
-    MIN_SPEED = 30
+    MAX_SPEED = 36
+    MIN_SPEED = 20
     
     def __init__(self, position: pygame.Vector2 | tuple[float, float], speed: float):
         self.speed = speed
         self.start_position = position
-        self.source = random.randint(0,1)
-        self.shadow = car.Car.create_shadow(Enemy.graphic.texture)
+        self.shadow = car.Car.create_shadow(Enemy.graphic)
         self.direction = pygame.Vector2()
+        self.rotation_offset = random.uniform(0, 180)
 
     def start(self):
         self.collider = collider.CircleCollider(pygame.Vector2(self.start_position), 4, "Enemy")
@@ -28,19 +29,21 @@ class Enemy(engine.Actor):
         self.collider.position += self.direction.normalize() * self.speed * engine.fixed_delta_time()
         
     def draw(self):
+        rotation = math.sin((engine.total_time + self.rotation_offset) * \
+                            remap(self.speed, Enemy.MIN_SPEED, Enemy.MAX_SPEED, 0.75, 7.5)) * 15
         # Draw shadow
         engine.draw_passes["Main"].blit(
             self.collider.position.y,
             self.shadow,
             self.collider.position + pygame.Vector2(1, 1),
-            source_rect=self.graphic[self.source]
+            rotation=rotation
         )
         # Draw self
         engine.draw_passes["Main"].blit(
             self.collider.position.y,
-            Enemy.graphic.texture if self.direction.x > 0 else pygame.transform.flip(Enemy.graphic.texture, True, False),
+            Enemy.graphic if self.direction.x > 0 else pygame.transform.flip(Enemy.graphic, True, False),
             self.collider.position,
-            source_rect=self.graphic[self.source] if self.direction.x > 0 else self.graphic[1 - self.source]
+            rotation=rotation
         )
 
     def end(self):
@@ -48,5 +51,8 @@ class Enemy(engine.Actor):
             20,
             Enemy.big_blood_tex.texture,
             self.collider.position,
-            source_rect=Enemy.big_blood_tex[random.randrange(0, Enemy.big_blood_tex.get_num_cells())]
+            (1.25, 1.25),
+            random.uniform(25, 65),
+            (0.5, 0.5),
+            Enemy.big_blood_tex[random.randrange(0, Enemy.big_blood_tex.get_num_cells())]
         )
